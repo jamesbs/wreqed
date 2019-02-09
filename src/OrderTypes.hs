373bdae58950bbe0bbfdbe54445ee0762b88e193
James Salas <osdev@jamesbs.com>
2019-02-09 00:03:27 -0500

    add post functionality

--- src/OrderTypes.hs
+++ src/OrderTypes.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module OrderTypes (
-  getOrderTypes
+    getOrderTypes
+  , postOrderTypes
 ) where
-import Network.Wreq (get, responseBody)
-import Control.Lens ((^?))
+import Network.Wreq
+import Control.Lens
+import Data.Aeson
+import Data.Aeson.Lens
 
 url = "http://localhost:3000/orderTypes"
 
-getOrderTypes = do
-  response <- (get url)
-  -- return response
-  return $ response ^? responseBody
+getOrderTypes = fmap (preview responseBody) (get url)
+
+postOrderTypes = post url [ "id" := ("LIMIT_EXPLOSION" :: String) ]
