922b22f92f68a7bc1809ef87026e8240f84344ea
James Salas <osdev@jamesbs.com>
2019-02-11 18:20:03 -0500

    attempt to inject function

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# Language GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DerivingStrategies #-}
 
 module Main where
 import Control.Monad.Reader
 import OrderTypes
 import Env
 import Network.Wreq
+import Data.String
 
-env = Env { endpoint = "http://localhost:3000" }
+env = Env { endpoint = "http://localhost:8888" }
+
+newtype EP = EP String deriving newtype Show
+
+-- instance Show EP where
+--   show (EP s) = s
+
+e = EP "http://localhost:3000"
 
 main :: IO()
 main = do
-  m <- runReaderT (postOrderTypes [ "id" := ("LIMIT_11" :: String) ]) env
+  print e
+  m <- runReaderT getOrderTypes ("http://localhost:8888" :: String)
   print m
 
--- src/OrderTypes.hs
+++ src/OrderTypes.hs
@@ -1,10 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ImpredicativeTypes #-}
 
-module OrderTypes (
-    getOrderTypes
-  , postOrderTypes
-) where
+module OrderTypes where
 import Network.Wreq
 import Control.Lens
 import Data.Aeson
@@ -31,4 +30,18 @@ getOrderTypes = do
 postOrderTypes :: (MonadReader env m, HasEndpoint env, MonadIO m) => [FormParam] -> m (Response ByteString)
 postOrderTypes body = do
   env <- ask
-  liftIO $ post ((getEndpoint env) ++ "/orderTypes") body
+  liftIO $ pot (getEndpoint env) body
+
+pot :: (MonadIO m) => String -> [FormParam] -> m (Response ByteString)
+pot endpoint params = liftIO $ post (endpoint++ "/orderTypes") params
+
+type GetOrderTypes m = (MonadIO m) => String -> m ByteString
+got :: (MonadIO m) => String -> m ByteString
+got endpoint = liftIO $ fmap (view responseBody) (get (endpoint ++ "/orderTypes"))
+
+component :: (MonadReader (GetOrderTypes m) m) => m ByteString
+component = do
+  g <- ask
+  g "string"
+  -- liftIO $ g "blah"
+
